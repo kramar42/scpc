cbe252aecc9793cbc00d26e45ae6444f8ac6e52f
Alexey Kramarenko <kramar42@gmail.com>
2021-01-15 21:25:53 +0100

    update bat scripts to generate glad files

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 if not exist build mkdir build
 
-set CFLAGS=/nologo /I"include" /Fo"build\\" /std:c11 /MD /O2 /Zc:inline /W4 /WX
+set CFLAGS=/nologo /I"include" /Fo"build\\" /std:c11 /MD /O2 /Zc:inline /W4 /WX /wd5105
 set LFLAGS=/nologo /ENTRY:"mainCRTStartup"
 rem /SUBSYSTEM:WINDOWS 
 set LIBS=lib\glfw3.lib opengl32.lib user32.lib gdi32.lib shell32.lib
-set SOURCES=main.c deps\glad_gl.c
+set SOURCES=src\main.c deps\*
 
 cl %CFLAGS% /c %SOURCES%
-link %LFLAGS% %LIBS% build\* /OUT:main.exe
+link %LFLAGS% %LIBS% build\*.obj /OUT:build\main.exe
--- install_deps.bat
+++ install_deps.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if "%~1" == "" echo "Specify GLFW location" && exit /b
+
 pushd .
 cd %1
 
@@ -10,18 +12,23 @@ msbuild GLFW.sln /p:Configuration=Release /p:Platform=x64
 popd
 
 rem copy lib file
-mkdir lib
+if not exist lib mkdir lib
 copy %1\src\Release\glfw3.lib lib
 
 rem copy headers
-mkdir include\GLFW
+if not exist include\GLFW mkdir include\GLFW
 copy %1\include\GLFW include\GLFW
 
-rem copy glad files
-mkdir include\glad
-copy %1\deps\glad include\glad
-mkdir deps
-copy %1\deps\glad_gl.c deps
+rem requires package to be installed: pip3 install glad
+rem generate glad files
+python3 -m glad --profile core --out-path %tmp%\glad --api gl=3.2 --generator c --spec gl
 
-rem copy other deps
-copy %1\deps\linmath.h include
+rem copy headers
+if not exist include\glad mkdir include\glad
+copy %tmp%\glad\include\glad include\glad
+if not exist include\KHR mkdir include\KHR
+copy %tmp%\glad\include\KHR include\KHR
+
+rem copy source
+if not exist deps mkdir deps
+copy %tmp%\glad\src deps
