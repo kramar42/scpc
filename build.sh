37a1d525901b1b6cb8d187bbd12f50f7aa359c86
Alexey Kramarenko <kramar42@gmail.com>
2021-01-15 21:28:22 +0100

    add -ldl to compile on linux

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir -p build
 
 PKGS="glfw3 gl"
 CFLAGS="-Wall -std=gnu11 -Iinclude `pkg-config --cflags $PKGS`"
-LIBS="`pkg-config --libs $PKGS` -lm"
-SOURCES="main.c deps/*"
+LIBS="`pkg-config --libs $PKGS` -lm -ldl"
+SOURCES="src/main.c deps/*"
 
-cc $CFLAGS $SOURCES $LIBS -o main
+cc $CFLAGS $SOURCES $LIBS -o build/main
